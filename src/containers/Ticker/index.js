10766d38accf838d2a1b084bdb8dae421c299680
James Didehvar <james@didehvar.com>
2018-05-13 13:10:27 +0100

    populate ticker rows from symbols

--- src/containers/Ticker/index.js
+++ src/containers/Ticker/index.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import TickerTable from '../../components/TickerTable';
 import TickerRow from '../../components/TickerRow';
 
-const Ticker = () => (
-  <TickerTable>
-    {[...Array(10).keys()].map(i => (
-      <TickerRow
-        key={i}
-        symbol="ETH"
-        last={Math.random() * 600}
-        changePercent={Math.random() * 3}
-        volume={Math.random() * 100000}
-      />
-    ))}
-  </TickerTable>
-);
+class Ticker extends Component {
+  state = { symbols: [] };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('/symbols');
+      this.setState({ symbols: await res.json() });
+    } catch (ex) {
+      console.error(ex);
+    }
+  }
+
+  render() {
+    const { symbols } = this.state;
+
+    return (
+      <TickerTable>
+        {symbols.map(symbol => (
+          <TickerRow
+            key={symbol}
+            symbol={symbol}
+            last={Math.random() * 600}
+            changePercent={Math.random() * 3}
+            volume={Math.random() * 100000}
+          />
+        ))}
+      </TickerTable>
+    );
+  }
+}
 
 export default Ticker;
